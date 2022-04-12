16aa4da659ea7d8499f9942ee9f333ba8045aacd
masud rana <masudranaacb@gmail.com>
2022-04-12 17:43:25 +0600

    please login to page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,14 +2,24 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 import app from '../../firebasse.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 const auth = getAuth(app);
 const Login = () => {
     const [signInWithGoogle, use] = useSignInWithGoogle(auth);
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    const handelSignInWithGoogle=()=>{
+        signInWithGoogle()
+        .then(()=>{
+            navigate(from, { replace: true });
+        })
+    }
     return (
         <div>
             <h4>please Login</h4>
             <div >
-                <button onClick={()=>signInWithGoogle()}>google sign-in</button>
+                <button onClick={() => handelSignInWithGoogle()}>google sign-in</button>
             </div>
             <form >
                 <input type="Email" placeholder='Email' />
